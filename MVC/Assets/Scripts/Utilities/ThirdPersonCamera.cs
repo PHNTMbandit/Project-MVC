2e543f7fdfa702d60c725671708e4bd4e63944b0
Domenic Pittari <dom.pittari@gmail.com>
2024-01-25 18:35:46 +0900

    Added y axis camera movement

--- MVC/Assets/Scripts/Utilities/ThirdPersonCamera.cs
+++ MVC/Assets/Scripts/Utilities/ThirdPersonCamera.cs
@@ -1,3 +1,4 @@
+using System;
 using MVC.Input;
 using Sirenix.OdinInspector;
 using UnityEngine;
@@ -17,8 +18,23 @@ namespace MVC.Utilities
 
         private void Update()
         {
-            float rotationAmount = _inputReader.LookInput.x * _rotateSpeed;
-            _followTarget.Rotate(Vector3.up, rotationAmount);
+            _followTarget.rotation *= Quaternion.AngleAxis(_inputReader.LookInput.x * _rotateSpeed, Vector3.up);
+            _followTarget.rotation *= Quaternion.AngleAxis(_inputReader.LookInput.y * _rotateSpeed, Vector3.left);
+
+            Vector3 angles = _followTarget.localEulerAngles;
+            angles.z = 0;
+            float angle = _followTarget.transform.localEulerAngles.x;
+
+            if (angle > 180 && angle < 340)
+            {
+                angles.x = 340;
+            }
+            else if (angle < 180 && angle > 40)
+            {
+                angles.x = 40;
+            }
+
+            _followTarget.localEulerAngles = angles;
         }
     }
 }
