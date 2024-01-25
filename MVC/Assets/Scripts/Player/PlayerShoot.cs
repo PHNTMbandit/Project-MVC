f5794856a96c0422177b3dbd1d0c40b60c8e58d8
Domenic Pittari <dom.pittari@gmail.com>
2024-01-25 19:06:19 +0900

    Added aiming

--- MVC/Assets/Scripts/Player/PlayerShoot.cs
+++ MVC/Assets/Scripts/Player/PlayerShoot.cs
@@ -7,7 +7,22 @@ namespace MVC.Player
     public class PlayerShoot : MonoBehaviour
     {
         [BoxGroup("References"), SerializeField]
+        private Transform _aimingTarget;
+
+        [BoxGroup("References"), SerializeField]
         private GameObject _bulletPrefab;
+
+        private Camera _camera;
+
+        private void Awake()
+        {
+            _camera = Camera.main;
+        }
+
+        public void Aim()
+        {
+            _aimingTarget.localEulerAngles = new(0, _camera.transform.localEulerAngles.y, 0);
+        }
 
         public void Shoot()
         {
