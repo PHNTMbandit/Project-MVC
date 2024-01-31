f610c0d85b6859d87912e6632ae57e3459dfab73
Domenic Pittari <dom.pittari@gmail.com>
2024-01-31 09:22:23 +0900

    Improved table inspector for better readability

--- MVC/Assets/Scripts/Controllers/CharacterDataController.cs
+++ MVC/Assets/Scripts/Controllers/CharacterDataController.cs
@@ -4,32 +4,22 @@ using UnityEngine;
 
 namespace MVC.Utilities
 {
+    [Serializable]
+    public class CharacterData
+    {
+        public string name;
+        public string spritePath;
+        public int spriteIndex;
+    }
+
     [CreateAssetMenu(fileName = "Character Data", menuName = "MVC/Character Data")]
     public class CharacterDataController : ScriptableObject
     {
-        [Serializable]
-        public class CharacterData
-        {
-            public string name;
-            public string spritePath;
-            public int spriteIndex;
-        }
-
         [Serializable, HideLabel]
         public class CharacterList
         {
-            public CharacterData c_0;
-            public CharacterData c_1;
-            public CharacterData c_2;
-            public CharacterData c_3;
-            public CharacterData c_4;
-            public CharacterData c_5;
-            public CharacterData c_6;
-            public CharacterData c_7;
-            public CharacterData c_8;
-            public CharacterData c_9;
-            public CharacterData c_10;
-            public CharacterData c_11;
+            [ReadOnly, TableList(AlwaysExpanded = true, DrawScrollView = false)]
+            public CharacterData[] characters;
         }
 
         [SerializeField, Required]
@@ -41,6 +31,11 @@ namespace MVC.Utilities
         private void OnEnable()
         {
             _characterList = JsonUtility.FromJson<CharacterList>(_JSONFile.text);
+        }
+
+        public CharacterData GetCharacterData(string name)
+        {
+            return Array.Find(_characterList.characters, i => i.name == name);
         }
     }
 }
