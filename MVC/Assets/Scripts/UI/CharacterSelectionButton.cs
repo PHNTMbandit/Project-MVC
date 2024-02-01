e3f4f8634c0000a731eefe2c1930c05d27cc07f4
Domenic Pittari <dom.pittari@gmail.com>
2024-02-01 14:59:19 +0900

    Added character selection capabilities

--- MVC/Assets/Scripts/UI/CharacterSelectionButton.cs
+++ MVC/Assets/Scripts/UI/CharacterSelectionButton.cs
@@ -1,3 +1,5 @@
+using MVC.Controllers;
+using MVC.Utilities;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,12 +7,32 @@ namespace MVC.UI
 {
     public class CharacterSelectionButton : MonoBehaviour
     {
+        public CharacterData Character { get; private set; }
+
         [SerializeField]
-        private Image _icon;
+        private Image _icon, _border;
+
+        [SerializeField]
+        private CharacterSelectionController _characterSelectionController;
+
+        public void SetCharacter(CharacterData character)
+        {
+            Character = character;
+        }
 
         public void SetIcon(Sprite sprite)
         {
             _icon.sprite = sprite;
+        }
+
+        public void SetBorderColour(Color colour)
+        {
+            _border.color = colour;
+        }
+
+        public void OnClick()
+        {
+            _characterSelectionController.SetCurrentCharacter(Character);
         }
     }
 }
---
+++ MVC/Assets/Scripts/UI/CharacterSelectionUI.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using MVC.Controllers;
+using MVC.Utilities;
+using Sirenix.OdinInspector;
+using TMPro;
+using UnityEngine;
+
+namespace MVC.UI
+{
+    public class CharacterSelectionUI : MonoBehaviour
+    {
+        [BoxGroup("Settings"), SerializeField]
+        private Color _defaultBorderColour, _selectedBorderColour;
+
+        [BoxGroup("Grid"), SerializeField]
+        private CharacterSelectionButton _templateSelectionButton;
+
+        [BoxGroup("Grid"), SerializeField]
+        private Transform _grid;
+
+        [BoxGroup("Information"), SerializeField]
+        private TextMeshProUGUI _currentCharacterName, _information;
+
+        [BoxGroup("References"), SerializeField]
+        private CharacterDataController _characterDataController;
+
+        [BoxGroup("References"), SerializeField]
+        private CharacterSelectionController _characterSelectionController;
+
+        private readonly List<CharacterSelectionButton> _buttons = new();
+
+        private void Awake()
+        {
+            _templateSelectionButton.gameObject.SetActive(false);
+
+            _characterSelectionController.onCharacterChange.AddListener(UpdateUI);
+        }
+
+        private void Start()
+        {
+            GenerateList();
+            UpdateUI();
+        }
+
+        public void GenerateList()
+        {
+            ResetList();
+
+            foreach (CharacterData character in _characterDataController.CharacterListData.characters)
+            {
+                CharacterSelectionButton button = Instantiate(_templateSelectionButton, _grid);
+                button.gameObject.SetActive(true);
+
+                button.SetCharacter(character);
+                button.SetIcon(Resources.Load<Sprite>(character.spritePath));
+
+                _buttons.Add(button);
+            }
+        }
+
+        private void ResetList()
+        {
+            if (_buttons.Count > 0)
+            {
+                foreach (CharacterSelectionButton button in _buttons)
+                {
+                    Destroy(button.gameObject);
+                }
+
+                _buttons.Clear();
+            }
+        }
+
+        private void UpdateUI()
+        {
+            CharacterData currentCharacter = _characterSelectionController.CurrentCharacter;
+            _currentCharacterName.SetText(currentCharacter.name);
+            _information.SetText($"Fire Rate: {currentCharacter.fireRate} \n Range: {currentCharacter.targetingRange} \n Speed: {currentCharacter.moveSpeed} Jump: {currentCharacter.jumpForce}");
+
+            _buttons.Find(i => i.Character == currentCharacter)?.SetBorderColour(_selectedBorderColour);
+            List<CharacterSelectionButton> unselectedCharacterButtons = _buttons.FindAll(i => i.Character != currentCharacter);
+
+            for (int i = 0; i < unselectedCharacterButtons.Count; i++)
+            {
+                unselectedCharacterButtons[i].SetBorderColour(_defaultBorderColour);
+            }
+        }
+    }
+}
