bd79f44ec8e0d56336fa533920adc15fab0b5b77
Domenic Pittari <dom.pittari@gmail.com>
2024-02-07 10:40:22 +0900

    Added player health bar

--- MVC/Assets/Scripts/UI/HealthBar.cs
+++ MVC/Assets/Scripts/UI/HealthBar.cs
@@ -1,5 +1,6 @@
 using MVC.Data;
 using Sirenix.OdinInspector;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,24 +9,46 @@ namespace MVC.UI
     [AddComponentMenu("UI/Bars/Health Bar")]
     public class HealthBar : MonoBehaviour
     {
-        public Health Target { get; private set; }
+        [BoxGroup("Settings"), ToggleLeft, SerializeField]
+        private bool _hasInstance, _hasAmountText;
+
+        [BoxGroup("Settings"), ShowIf("_hasInstance"), SerializeField]
+        private Health _target;
+
+        [BoxGroup("Settings"), ShowIf("_hasAmountText"), SerializeField]
+        private TextMeshProUGUI _amountText;
 
         [BoxGroup("References"), SerializeField]
         private Slider _slider;
 
+        private void Awake()
+        {
+            if (_hasInstance)
+            {
+                UpdateSlider();
+
+                _target.onHealthChanged.AddListener(UpdateSlider);
+            }
+        }
+
         public void Initialise(Health target)
         {
-            Target = target;
+            _target = target;
 
             UpdateSlider();
 
-            Target.onHealthChanged.AddListener(UpdateSlider);
+            _target.onHealthChanged.AddListener(UpdateSlider);
         }
 
         private void UpdateSlider()
         {
-            _slider.maxValue = Target.MaxHealth;
-            _slider.value = Target.CurrentHealth;
+            _slider.maxValue = _target.MaxHealth;
+            _slider.value = _target.CurrentHealth;
+
+            if (_hasAmountText)
+            {
+                _amountText.SetText($"{_target.CurrentHealth}/{_target.MaxHealth}");
+            }
         }
     }
 }
